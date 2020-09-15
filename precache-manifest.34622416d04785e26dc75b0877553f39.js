cb0420d89d2a7ff59b87a31f358a14f35f544c4f
Cindy Zhang <cindyzhang0718@gmail.com>
2020-09-14 21:35:53 -0400

    Updates

--- precache-manifest.34622416d04785e26dc75b0877553f39.js
+++ precache-manifest.34622416d04785e26dc75b0877553f39.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f7ab92179cf9749a3d5d8f6ee4e42fc",
+    "revision": "99ae3afe0a5b2c820f87bff99f3df8f4",
     "url": "/index.html"
   },
   {
-    "revision": "597c2fac799edbb1230e",
-    "url": "/static/css/main.3b4729f3.chunk.css"
+    "revision": "b8072cb93bfc68258200",
+    "url": "/static/css/main.d62243cb.chunk.css"
   },
   {
     "revision": "bfb009a40619a0a77397",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.40afadff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "597c2fac799edbb1230e",
-    "url": "/static/js/main.f0c85e1d.chunk.js"
+    "revision": "b8072cb93bfc68258200",
+    "url": "/static/js/main.5ff95717.chunk.js"
   },
   {
     "revision": "2810e38988d32dc835b2",
