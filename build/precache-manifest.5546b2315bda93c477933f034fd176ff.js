f1edf36dcec246c74f04af99ec7c906d2af23954
Cindy <cindyzhang7@berkeley.edu>
2021-08-06 00:30:14 -0700

    Added build

---
+++ build/precache-manifest.5546b2315bda93c477933f034fd176ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7bd36b3b716d3ef349273bc5a1170a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bf733d3390f52a0520b",
+    "url": "/static/css/main.370b129e.chunk.css"
+  },
+  {
+    "revision": "7a4b010b7d400c8b993f",
+    "url": "/static/js/2.3532a47b.chunk.js"
+  },
+  {
+    "revision": "2c3f87656749125fd7ec040187420717",
+    "url": "/static/js/2.3532a47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf733d3390f52a0520b",
+    "url": "/static/js/main.fe01bb37.chunk.js"
+  },
+  {
+    "revision": "2810e38988d32dc835b2",
+    "url": "/static/js/runtime-main.01b49652.js"
+  },
+  {
+    "revision": "eebdb99bd2e58c4745f82414f7d53033",
+    "url": "/static/media/email.eebdb99b.svg"
+  },
+  {
+    "revision": "3388e633adf1b51ec9b4174841f8d009",
+    "url": "/static/media/github.3388e633.svg"
+  },
+  {
+    "revision": "4a388ea235c3f4b8be20853a7a6fb83f",
+    "url": "/static/media/glific.4a388ea2.png"
+  },
+  {
+    "revision": "ac76be104a7a4d15a9249e80c06278c6",
+    "url": "/static/media/linkedin.ac76be10.svg"
+  },
+  {
+    "revision": "f9eb42f7fbeecf51c142a33b7f4daba4",
+    "url": "/static/media/ppower.f9eb42f7.png"
+  },
+  {
+    "revision": "05beef8c5d5719dffd251e52da811d14",
+    "url": "/static/media/wnrs.05beef8c.png"
+  }
+]);
